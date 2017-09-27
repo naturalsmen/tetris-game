bb535c8addc452cbc197e29c253511b6390cd41e
Eric Hung <naturalsmen@gmail.com>
2017-09-27 21:56:43 +0800

    Isolated main from Tetris.java
    This is the first dismantling which isolates "main" from Tetris.java and
    a new "Main.java" file is created.

---
+++ src/tetris/Main.java
@@ -0,0 +1,10 @@
+package tetris;
+
+public class Main {
+	public static void main(String[] args) {
+
+        Tetris game = new Tetris();
+        game.setLocationRelativeTo(null);
+        game.setVisible(true);
+	}
+}
